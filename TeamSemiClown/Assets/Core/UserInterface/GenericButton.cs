a0459314f6e7b40626e58c05a3592677eed5a76a
AlexMah <alexmahjy@gmail.com>
2025-07-12 19:22:45 +0800

    Setup Managers
    - AudioManager
    - SceneTransitionManager
    - Imported Assets (Font, BG)
    - Cleaned up Editor stuff

---
+++ TeamSemiClown/Assets/Core/UserInterface/GenericButton.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class GenericButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+    [Header("Scale Settings")]
+    [SerializeField] private float defaultScale = 1f;
+    [SerializeField] private float hoverScale = 1.1f;
+    [SerializeField] private float clickScale = 1.2f;
+    [SerializeField] private float tweenDuration = 0.15f;
+
+    [Header("Additional OnClick Settings")]
+    [SerializeField] private AudioClip audioToPlay;
+
+    private Vector3 originalScale;
+    private Tween currentTween;
+
+    void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ScaleTo(hoverScale);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ScaleTo(defaultScale);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(audioToPlay)
+        {
+            SFXController.Instance.PlaySoundFXClip(audioToPlay, transform, 1.0f);
+        }
+
+        // Reset everything, play a ferent scale animation for click
+        currentTween?.Kill();
+        transform.DOKill();
+
+        Sequence clickSequence = DOTween.Sequence();
+        clickSequence.Append(transform.DOScale(originalScale * clickScale, tweenDuration / 2).SetEase(Ease.OutQuad));
+        clickSequence.Append(transform.DOScale(originalScale * hoverScale, tweenDuration / 2).SetEase(Ease.InQuad));
+        clickSequence.Play();
+    }
+
+    private void ScaleTo(float scaleFactor)
+    {
+        // Reset any existing tweens
+        currentTween?.Kill();
+        transform.DOKill();
+        currentTween = transform.DOScale(originalScale * scaleFactor, tweenDuration).SetEase(Ease.OutBack);
+    }
+}
