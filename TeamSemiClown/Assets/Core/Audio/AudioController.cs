f6130b5a7b8df64f552539a2aae1a0a63af5dacc
AlexMah <alexmahjy@gmail.com>
2025-07-13 15:26:43 +0800

    Updated UI, Updated MainMenu, Imported new assets

--- TeamSemiClown/Assets/Core/Audio/AudioController.cs
+++ TeamSemiClown/Assets/Core/Audio/AudioController.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class AudioController : MonoBehaviour
 {
+    [Header("Audio Settings")]
     [SerializeField] private AudioMixer audioMixer;
     [SerializeField] private Slider masterVolumeSlider;
     [SerializeField] private Slider musicVolumeSlider;
     [SerializeField] private Slider sfxVolumeSlider;
 
+    [Header("Pause Configs")]
+    [SerializeField] private GameObject pauseButton;
+    [SerializeField] private bool EnableInGameLevel = true;
+
     private const string MasterVolumeKey = "MasterVolume";
     private const string MusicVolumeKey = "MusicVolume";
     private const string SFXVolumeKey = "SFXVolume";
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
 
     private void Start()
     {
@@ -57,5 +73,17 @@ public class AudioController : MonoBehaviour
     private float LinearToDecibel(float linear)
     {
         return linear > 0.0001f ? Mathf.Log10(linear) * 20f : -80f;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (EnableInGameLevel && scene.buildIndex == (int)SceneType.Game)
+        {
+            pauseButton.SetActive(true);
+        }
+        else
+        {
+            pauseButton.SetActive(false);
+        }
     }
 }
